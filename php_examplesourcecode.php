f550875658fc87036fd9cc90dec4119264990317
ericaandrews <eureka.web.dev@gmail.com>
2018-05-23 16:37:09 -0700

    Add files via upload

---
+++ php_examplesourcecode.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head><title>PHP Programming_PHP Example</title></head>   
+<body>
+    <code>
+        &lt;html&gt;<br />
+        &lt;head&gt;<br />
+        &nbsp;&lt;title&gt;PHP Programming_PHP Example&lt;/title&gt;<br />
+        &lt;/head&gt;<br />
+        &lt;body&gt;<br />
+        &nbsp;&lt;?php<br />
+        &nbsp;&#36;hoursWorked&nbsp;&#61;&nbsp;3&#59;<br />
+        &nbsp;&#36;payRate&nbsp;&#61;&nbsp;52.1429&#59;<br />
+        &nbsp;&#36;grossPay&nbsp;&#61;&nbsp;&#36;hoursWorked&nbsp;&#42;&nbsp;&#36;payRate&#59;<br />
+        &nbsp;print "&lt;p&gt;Hours Worked&#58;&nbsp;&#36;hoursWorked&lt;/p&gt;"&#59;<br />
+        &nbsp;print "&lt;p&gt;Pay Rate&#58;&nbsp;&#36;payRate&lt;/p&gt;"&#59;<br />
+        &nbsp;print "&lt;p&gt;Gross Pay&#58;&nbsp;&#36;grossPay&lt;/p&gt;"&#59;<br />
+        &nbsp;&#63;&gt;<br />
+        &lt;/body&gt;<br />
+        &lt;/html&gt;<br />
+    </code>
+</body>    
+</html>
