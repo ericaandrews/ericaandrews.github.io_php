64353560d67b35102c98b1a5d0c3e1dbcc74babf
ericaandrews <eureka.web.dev@gmail.com>
2018-05-23 16:35:36 -0700

    Add files via upload

---
+++ php_example.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head><title>PHP Programming_PHP Example</title></head>   
+<body>
+    <?php 
+    $hoursWorked = 3;
+    $payRate = 52.1429;
+    $grossPay = $hoursWorked * $payRate;
+    
+    print "<p>Hours Worked: $hoursWorked</p>";
+    print "<p>Pay Rate: $payRate</p>";
+    print "<p>Gross Pay: $grossPay</p>";
+    ?>
+    
+</body>    
+</html>
