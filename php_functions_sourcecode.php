c937e91d81ea1a3bf38a788f3801053910f17679
ericaandrews <eureka.web.dev@gmail.com>
2018-05-23 17:45:03 -0700

    Update php_functions_sourcecode.php

--- php_functions_sourcecode.php
+++ php_functions_sourcecode.php
@@ -21,7 +21,6 @@
              &nbsp;&#125;<br />
              &nbsp;&#63;&gt;<br />
              &nbsp;&lt;br&gt;&lt;br&gt;<br />    
-        &nbsp;&lt;a&nbsp;href&#61;"https&#58;//hills&#46;ccsf&#46;edu/&#126;eandrew6/php&#95;functions&#46;html"&gt;Click&nbsp;here&nbsp;to&nbsp;return&nbsp;to&nbsp;the&nbsp;original&nbsp;form&lt;/a&gt;&lt;br/&gt;<br/>
         &lt;/body&gt;<br />
         &lt;/html&gt;<br />
     </code>
